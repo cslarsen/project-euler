9d1229909334e56c468680aa4d1957a5b0e31b0d
Christian Stigen Larsen <csl@sublevel3.org>
2012-11-08 20:10:27 +0100

    Works for 3-gon

--- e68/e68.cpp
+++ e68/e68.cpp
@@ -1,47 +1,65 @@
 /*
  * ProjectEuler.net problem 68
  * Solved by Christian Stigen Larsen
+ *
+ * Magic 3-gon:
+ *
+ *      O 4
+ *       \
+ *        O 3
+ *       / \
+ *    1 O---O--O 6
+ *     /   2
+ *  5 O
+ *
+ * For the magic 5-gon, remember that 10 must be in the outer nodes.
  */
 
 #include <iostream>
 #include <set>
+#include <algorithm>
 #include <inttypes.h>
-
-#define SIZE 3
-typedef uint32_t I;
 
 using namespace std;
 
-static set<I> range(const I a, const I b)
+typedef uint32_t I;
+typedef set<I> SET;
+
+std::ostream& operator<<(std::ostream& o, const set<I>& s)
 {
-  set<I> r;
+  for ( const auto& x : s )
+    o << x << " ";
 
-  for ( I i=a; i<=b; ++i )
-    r.insert(i);
-
-  return r;
+  return o;
 }
 
-static I sum(const set<I>& s)
-{
-  I r = 0;
-
-  for ( set<I>::const_iterator i = s.begin();
-        i != s.end(); ++i )
-  {
-    r += *i;
-  }
-
-  return r;
-}
 
 int main()
 {
-  I inner[SIZE];
-  I outer[SIZE];
+  I inner[3];
+  I outer[3];
+  I digits[] = {1,2,3,4,5,6};
 
-  set<I> s = range(1, SIZE*2);
+  do {
+    inner[0] = digits[0];
+    inner[1] = digits[1];
+    inner[2] = digits[2];
 
-  I minsum = sum(range(1, SIZE));
-  I maxsum = sum(range(SIZE+1, SIZE*2));
+    outer[0] = digits[3];
+    outer[1] = digits[4];
+    outer[2] = digits[5];
+
+    I sum1 = outer[0] + inner[0] + inner[1];
+    I sum2 = outer[1] + inner[1] + inner[2];
+    I sum3 = outer[2] + inner[2] + inner[0];
+
+    if ( sum1 == sum2 && sum3 == sum2 ) {
+      cout.width(2);
+      cout << sum1 << " ";
+      cout << outer[0] << "," << inner[0] << "," << inner[1] << ";";
+      cout << outer[1] << "," << inner[1] << "," << inner[2] << ";";
+      cout << outer[2] << "," << inner[2] << "," << inner[0] << endl;
+    }
+
+  } while ( next_permutation(digits, digits+sizeof(digits)/sizeof(I)) );
 }
