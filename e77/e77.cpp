3a0a045c14fddb207569bf3db94ca33c30a2f6c5
Christian Stigen Larsen <csl@sublevel3.org>
2012-11-12 22:13:07 +0100

    Added problem 77

---
+++ e77/e77.cpp
@@ -0,0 +1,15 @@
+/*
+ * ProjectEuler.net problem 77
+ * Solved by Christian Stigen Larsen
+ */
+
+#include <iostream>
+#include "primes.h"
+
+using namespace std;
+
+typedef uint32_t Z;
+
+int main()
+{
+}
