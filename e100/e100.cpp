de087f63226ef0407d13db5e23b6fe645f62f680
Christian Stigen Larsen <csl@sublevel3.org>
2012-11-16 00:33:22 +0100

    Simplified

--- e100/e100.cpp
+++ e100/e100.cpp
@@ -2,20 +2,20 @@
  * ProjectEuler.net problem 100
  * Solved by Christian Stigen Larsen
  *
- * The chance is given as
+ * The probability is
  *
  *   P(BB) = b/(b+r) * (b-1)/(b+r-1) = 1/2
  *
- * This is the same as
+ * Rearranged, we get
  *
  *   2*b*(b-1) = (b+r)*(b+r-1)
  *
- * Solved for r we get from WolframAlpha:
+ * Solving for r (Wolfram Alpha) gives
  *
- *   0.5 * (sqrt(8*r*r+1) + 2*r + 1)
+ *   b = (sqrt(8*r*r+1) + 2*r + 1) / 2
  *
- * So a fast check to see if r is an acceptable number is
- * to see if sqrt(8*r*r+1) is an integer.
+ * So a fast check to see if r is an acceptable number is to see if
+ * sqrt(8*r*r+1) is an integer.
  *
  * Example output:
  *
@@ -47,68 +47,13 @@
 using namespace std;
 typedef __uint128_t Z;
 
-static inline bool good_r(const Z& r)
-{
-  const Z rad = 8*r*r+1;
-  const Z s = sqrt(rad);
-  return s*s == rad;
-}
-
-static inline bool solution(const Z& r, const Z& b)
-{
-  return 2*b*(b-1) == (b+r)*(b+r-1);
-}
-
-static inline Z solve_b(const Z& r)
-{
-  return (sqrt(8*r*r+1) + 2*r + 1) / 2;
-}
+static Z interval = 1e8;
+static Z threads = omp_get_num_threads();
 
 static ostream& operator<<(ostream& o, const __uint128_t& n)
 {
-  if ( n < 0xFFFFFFFFFFFFFFFF )
-    return o << static_cast<uint64_t>(n);
-
-  o << "0x" << static_cast<uint64_t>(n>>64) << static_cast<uint64_t>(n);
-  return o;
+  return o << static_cast<uint64_t>(n);
 }
-
-static void find(const Z& start, const Z& omp_interval)
-{
-  assert(start>0);
-
-  /*
-   * Benchmark: r<=271669860 should take about 3.5-4.0 secs
-   *
-   * The reason we want such a huge number of threads is to
-   * avoid the threads in the lower space to stop too quickly.
-   */
-
-  const Z STOP = 1e12;
-  const Z MARK = 1e12;
-
-  #pragma omp parallel for schedule(dynamic, omp_interval)
-  for ( Z r=start; r<STOP; ++r ) {
-    if ( !good_r(r) )
-      continue;
-
-    const Z b = solve_b(r);
-
-    if ( !solution(r, b) )
-      continue;
-
-    int i = omp_get_thread_num()+1;
-    int j = omp_get_num_threads();
-
-    #pragma omp critical
-    cout << i << "/" << j << " " // thread no.
-         << "r=" << r << " b=" << b << " r+b=" << b+r
-         << ((r+b)>=MARK? " quack!" : "") << endl;
-  }
-}
-
-static Z interval = 1e6;
-static Z threads = 4*omp_get_num_threads();
 
 static void help()
 {
@@ -120,22 +65,49 @@ static void help()
 int main(int argc, char** argv)
 {
   if ( argc > 1 ) {
-    if ( !isdigit(argv[1][0]) )
-      help();
+    if ( !isdigit(argv[1][0]) ) help();
+    else threads = atoi(argv[1]);
 
-    threads = atoi(argv[1]);
-  }
-
-  if ( argc > 2 ) {
-    if ( !isdigit(argv[2][0]) )
-      help();
-
-    interval = atoi(argv[2]);
+    if ( argc > 2 ) {
+      if ( !isdigit(argv[2][0]) ) help();
+      else interval = atoi(argv[2]);
+    }
   }
 
   cout << "interval " << interval << endl;
   omp_set_num_threads(threads);
 
-  find(1, interval);
-  return 0;
+  /*
+   * Benchmark: r<=271669860 should take about 3.5-4.0 secs
+   *
+   * The reason we want such a huge number of threads is to
+   * avoid the threads in the lower space to stop too quickly.
+   */
+
+  const Z STOP = 1e13;
+  const Z MARK = 1e12;
+
+  // checkpoint, use r=start=53789260175
+
+  #pragma omp parallel for schedule(dynamic, interval)
+  for ( Z r=1; r<STOP; ++r ) {
+    const Z rad = 8*r*r+1;
+    const Z s = sqrt(rad);
+
+    if ( s*s != rad )
+      continue; // not a perfect square
+
+    const Z b = (s + 2*r + 1) / 2;
+
+    if ( b*(b-1) != r*(2*b+r-1) ) // 3 muls, 3 adds
+      continue;
+
+    const int i = omp_get_thread_num()+1;
+    const int j = omp_get_num_threads();
+
+    #pragma omp critical
+    cout << i << "/" << j << " " // thread no.
+         << "r=" << r << " b=" << b << " r+b=" << b+r
+         << ((r+b)>=MARK? " quack!" : "") << endl;
+  }
 }
