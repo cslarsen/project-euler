bc5ab1d83d4e0fd0f3c31df5b47e26c9a4f6fdb0
Christian Stigen Larsen <csl@sublevel3.org>
2012-11-15 22:11:40 +0100

    Changed to OpenMP

--- e100/e100.cpp
+++ e100/e100.cpp
@@ -16,61 +16,81 @@
  * r=7997214 b=19306983 r+b=27304197
  * r=46611179 b=112529341 r+b=159140520
  * r=271669860 b=655869061 r+b=927538921
- *
- * Using uint64_t instead of mpz_class causes incorrect results:
- *
- * r=4294967296 b=4294967297 r+b=8589934593
- * r=8589934592 b=8589934593 r+b=17179869185
- * r=8589936640 b=8598325249 r+b=17188261889
+ * r=1583407981 b=3822685023 r+b=5406093004
+ * r=9228778026 b=22280241075 r+b=31509019101
  *
  */
 
 #include <iostream>
+#include <cassert>
 #include <inttypes.h>
-#include <gmpxx.h>
+#include <math.h>
+#include <omp.h>
 
 using namespace std;
-typedef mpz_class Z;
+typedef __uint128_t Z;
 
 static inline bool good_r(const Z& r)
 {
   const Z rad = 8*r*r+1;
   const Z s = sqrt(rad);
-
-  // is s an integer?
   return s*s == rad;
 }
 
-static bool correct(const Z& r, const Z& b)
+static inline bool solution(const Z& r, const Z& b)
 {
-  return b>1 && r+b>1 && 2*b*(b-1) == (b+r)*(b+r-1);
+  return 2*b*(b-1) == (b+r)*(b+r-1);
 }
 
-static Z solve_b(const Z& r)
+static inline Z solve_b(const Z& r)
 {
   return (sqrt(8*r*r+1) + 2*r + 1) / 2;
 }
 
+static ostream& operator<<(ostream& o, const __uint128_t& n)
+{
+  if ( n < 0xFFFFFFFFFFFFFFFF )
+    return o << static_cast<uint64_t>(n);
+
+  o << "0x" << static_cast<uint64_t>(n>>64) << static_cast<uint64_t>(n);
+  return o;
+}
+
 static void find(const Z& start)
 {
-  const Z STOP = 1e12;
+  assert(start>0);
 
-  for ( Z r=start; ; ++r ) {
+  /*
+   * Benchmark: r<=271669860 should take about 3.5-4.0 secs
+   *
+   * The reason we want such a huge number of threads is to
+   * avoid the threads in the lower space to stop too quickly.
+   */
+
+  const Z STOP = 1e12;
+  const Z MARK = 1e12;
+
+  #pragma omp parallel for schedule(dynamic, 10000000) num_threads(16)
+  for ( Z r=start; r<STOP; ++r ) {
     if ( !good_r(r) )
       continue;
 
     const Z b = solve_b(r);
 
-    if ( correct(r, b) ) {
-      cout << "r=" << r << " b=" << b << " r+b=" << b+r << endl;
+    if ( !solution(r, b) )
+      continue;
 
-      if ( r+b > STOP )
-        return;
-    }
+    int i = omp_get_thread_num()+1;
+    int j = omp_get_num_threads();
+
+    #pragma omp critical
+    cout << i << "/" << j << " " // thread no.
+         << "r=" << r << " b=" << b << " r+b=" << b+r << " "
+         << (r+b>MARK? "*" : "") << endl;
   }
 }
 
 int main()
 {
-  find(271669860-1);
+  find(1);
 }
