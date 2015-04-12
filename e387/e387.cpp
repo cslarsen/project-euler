869fd5f9900a62926e29a58c17fc12d59eb2481f
Christian Stigen Larsen <csl@csl.name>
2015-04-12 13:29:13 +0200

    Adds prime sieve

--- e387/e387.cpp
+++ e387/e387.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
+#include "prime_sieve.h"
 
 typedef uint64_t Z;
+#define LIMIT 100000000
 
-static Z digitsum(Z n)
+prime_sieve<LIMIT> *sieve = NULL;
+
+static inline Z digitsum(Z n)
 {
   Z s = 0;
 
@@ -16,13 +20,15 @@ static Z digitsum(Z n)
   return s;
 }
 
-static bool harshad(const Z& n)
+static inline bool harshad(const Z& n)
 {
   return (n % digitsum(n)) == 0;
 }
 
-static bool isprime(const Z& n)
+static inline bool isprime(const Z& n)
 {
+  return sieve->isprime(n);
+  /*
   if ( n <= 1 )
     return false;
 
@@ -31,14 +37,15 @@ static bool isprime(const Z& n)
       return false;
 
   return true;
+  */
 }
 
-static bool strong_harshad(const Z& n)
+static inline bool strong_harshad(const Z& n)
 {
   return n <= 1 ? false : isprime(n / digitsum(n));
 }
 
-static bool rt_harshad(Z n)
+static inline bool rt_harshad(Z n)
 {
   for ( ; n > 1; n /= 10 )
     if ( !harshad(n) )
@@ -47,7 +54,7 @@ static bool rt_harshad(Z n)
   return true;
 }
 
-static bool strong_rt_harshad(const Z& n)
+static inline bool strong_rt_harshad(const Z& n)
 {
   if ( !isprime(n) )
     return false;
@@ -59,24 +66,49 @@ static Z srt_sum(const Z& limit)
 {
   Z s = 0;
 
-  for ( Z n=1; n < limit; ++n )
+  for ( Z n=1; n < limit; ++n ) {
     if ( strong_rt_harshad(n) )
       s += n;
+
+    if ( (n % 25000000) == 0 ) {
+      printf("n=%llu sum=%llu\n", n, s);
+      fflush(stdout);
+    }
+  }
 
   return s;
 }
 
-int main()
+static bool test()
 {
   Z s = srt_sum(10000);
+
   if ( s != 90619 ) {
-    printf("srt_sum(10000) == %llu != 90619n", s);
-    return 1;
+    printf("test failed: srt_sum(10000) ==> %llu != 90619\n", s);
+    return false;
   }
 
-  const Z limit = 10000000;
-  printf("limit=%llu\ncalculating ... ", limit);
+  printf("test ok\n");
+  return true;
+}
+
+
+int main()
+{
+  const Z limit = LIMIT;
+
+  printf("finding primes < %llu ... ", limit);
   fflush(stdout);
-  printf("%llu\n", srt_sum(10000000));
+  sieve = new prime_sieve<limit>();
+  printf("%llu primes\n", sieve->primes);
+  fflush(stdout);
+
+  if ( !test() )
+    return 1;
+
+  printf("calculating ... ");
+  fflush(stdout);
+  printf("sum < %llu = %llu\n", limit, srt_sum(limit));
+
   return 0;
 }
